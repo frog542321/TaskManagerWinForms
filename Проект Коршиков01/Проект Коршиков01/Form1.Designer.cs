15f41efa4cca88c6ea238be47f4b0f6b82a83d9d
Александр Коршиков <sun66884@gmail.com>
2026-04-30 13:47:07 +0300

    xfgjdfhgj

--- Проект Коршиков01/Проект Коршиков01/Form1.Designer.cs
+++ Проект Коршиков01/Проект Коршиков01/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -47,6 +48,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,6 +61,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Удалить выбранные";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -68,6 +71,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "Редактировать";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -75,6 +79,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(212, 26);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -111,11 +116,21 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Сортировать по:";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(403, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "sdfghdfghj";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
@@ -141,6 +156,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Проект Коршиков01/Проект Коршиков01/Form1.cs
+++ Проект Коршиков01/Проект Коршиков01/Form1.cs
@@ -12,12 +12,38 @@ namespace Проект_Коршиков01
 {
     public partial class Form1 : Form
     {
+        Task[] tasks = new Task[0];
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void LightGreen(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task tasks = new Task(textBox1.Text, Convert.ToDouble(textBox1.Text));
+            listBox1.Items.Add(tasks.Print());
+            textBox1.Text.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double notclickbutton = 0;
+            listBox1.Items.Clear();
+            foreach (var n in tasks)
+            {
+                listBox1.Items.Add(n.Print());
+                notclickbutton += n.notclick();
+            }
+            label3.Text = Convert.ToString($"{notclickbutton}");
+            listBox1.Items.Add(tasks.Print());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
         {
 
         }
