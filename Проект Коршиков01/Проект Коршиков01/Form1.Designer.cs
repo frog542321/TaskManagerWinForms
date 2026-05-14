045a32c136893998a8d84b4acd15a715121db611
Александр Коршиков <sun66884@gmail.com>
2026-05-14 16:30:28 +0300

    serthdfghkdghkgj

--- Проект Коршиков01/Проект Коршиков01/Form1.Designer.cs
+++ Проект Коршиков01/Проект Коршиков01/Form1.Designer.cs
@@ -34,16 +34,21 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightGreen;
-            this.button1.Location = new System.Drawing.Point(21, 50);
+            this.button1.Location = new System.Drawing.Point(10, 440);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 32);
             this.button1.TabIndex = 1;
@@ -56,7 +61,7 @@
             this.button2.BackColor = System.Drawing.Color.LightCoral;
             this.button2.Cursor = System.Windows.Forms.Cursors.Default;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(131, 50);
+            this.button2.Location = new System.Drawing.Point(121, 440);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(193, 32);
             this.button2.TabIndex = 2;
@@ -66,7 +71,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(350, 53);
+            this.button3.Location = new System.Drawing.Point(12, 499);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(137, 30);
             this.button3.TabIndex = 3;
@@ -94,38 +99,21 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(12, 107);
+            this.listBox1.Location = new System.Drawing.Point(479, 137);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(352, 404);
             this.listBox1.TabIndex = 6;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(582, 20);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 7;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(440, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(136, 20);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Сортировать по:";
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(407, 107);
+            this.textBox2.Location = new System.Drawing.Point(479, 33);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(212, 26);
             this.textBox2.TabIndex = 10;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(407, 158);
+            this.button4.Location = new System.Drawing.Point(479, 65);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(137, 30);
             this.button4.TabIndex = 11;
@@ -133,15 +121,82 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 209);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 208);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(251, 50);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(180, 24);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Готова ли задача?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 50);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Приоритет:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(154, 155);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Сортировать по:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 544);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(869, 582);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -163,10 +218,15 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Проект Коршиков01/Проект Коршиков01/Form1.cs
+++ Проект Коршиков01/Проект Коршиков01/Form1.cs
@@ -23,6 +23,14 @@ namespace Проект_Коршиков01
         {
             string task = textBox1.Text.ToString();
             listBox1.Items.Add(task);
+
+            Task task1 = new Task
+            {
+                Title = textBox1.Text,
+                Description = richTextBox1.Text,
+                Date = dateTimePicker1.Value,
+                Priority = (TaskPriority)Enum.Parse(typeof(TaskPriority), comboBox1.Text),
+            };
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -48,15 +56,50 @@ namespace Проект_Коршиков01
                 MessageBox.Show("Не выбрана задача для выполнения действия");
                 return;
             }
+
+            int index = listBox1.SelectedIndex;
+            Task tasks = task1[index];
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
 
         }
+        private void listBox1_SlectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is Task selected)
+            {
+                textBox1.Text = selected.Title;
+                richTextBox1.Text = selected.Description;
+                dateTimePicker1.Text = selected.Date;
+                comboBox1.Text = selected.Priority.ToString();
+                checkBox1.Checked = false;
+                if (comboBox1.Items.Count > 0) comboBox1.SelectedIndex = 0;
+            }
+        }
+        public enum TaskPriority { Low, Medium, High }
         public class Task
         {
             public string Title { get; set; }
+            public string Description { get; set; } 
+            public DateTime Date { get; set; }
+            public TaskPriority Priority { get; set; }
+            public bool Completed { get; set; }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
