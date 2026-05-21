bb3c6cadecac45074307cd0d26a20cc765b4640d
Анара Ахметова <anaraibragimovna@gmail.com>
2026-05-21 14:40:00 +0300

    готово3!!!!!!!

--- Проект Коршиков01/Проект Коршиков01/Form1.Designer.cs
+++ Проект Коршиков01/Проект Коршиков01/Form1.Designer.cs
@@ -60,10 +60,9 @@
             // btnAdd
             // 
             this.btnAdd.BackColor = System.Drawing.Color.LightGreen;
-            this.btnAdd.Location = new System.Drawing.Point(5, 18);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Location = new System.Drawing.Point(8, 28);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(129, 25);
+            this.btnAdd.Size = new System.Drawing.Size(194, 38);
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "Добавить";
             this.btnAdd.UseVisualStyleBackColor = false;
@@ -74,10 +73,9 @@
             this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
             this.btnDelete.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnDelete.Location = new System.Drawing.Point(5, 43);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Location = new System.Drawing.Point(8, 66);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(129, 25);
+            this.btnDelete.Size = new System.Drawing.Size(194, 38);
             this.btnDelete.TabIndex = 2;
             this.btnDelete.Text = "Удалить выбранные";
             this.btnDelete.UseVisualStyleBackColor = false;
@@ -85,10 +83,9 @@
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(5, 70);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEdit.Location = new System.Drawing.Point(8, 108);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(129, 25);
+            this.btnEdit.Size = new System.Drawing.Size(194, 38);
             this.btnEdit.TabIndex = 3;
             this.btnEdit.Text = "Редактировать";
             this.btnEdit.UseVisualStyleBackColor = true;
@@ -96,67 +93,62 @@
             // 
             // txtTitle
             // 
-            this.txtTitle.Location = new System.Drawing.Point(66, 13);
-            this.txtTitle.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTitle.Location = new System.Drawing.Point(99, 20);
             this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(143, 20);
+            this.txtTitle.Size = new System.Drawing.Size(212, 26);
             this.txtTitle.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 16);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(8, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "Название";
             // 
             // chkCompleted
             // 
             this.chkCompleted.AutoSize = true;
-            this.chkCompleted.Location = new System.Drawing.Point(4, 91);
-            this.chkCompleted.Margin = new System.Windows.Forms.Padding(2);
+            this.chkCompleted.Location = new System.Drawing.Point(6, 140);
             this.chkCompleted.Name = "chkCompleted";
-            this.chkCompleted.Size = new System.Drawing.Size(120, 17);
+            this.chkCompleted.Size = new System.Drawing.Size(180, 24);
             this.chkCompleted.TabIndex = 13;
             this.chkCompleted.Text = "Готова ли задача?";
             this.chkCompleted.UseVisualStyleBackColor = true;
             // 
             // dtpDate
             // 
-            this.dtpDate.Location = new System.Drawing.Point(5, 37);
-            this.dtpDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpDate.Location = new System.Drawing.Point(8, 57);
             this.dtpDate.Name = "dtpDate";
-            this.dtpDate.Size = new System.Drawing.Size(135, 20);
+            this.dtpDate.Size = new System.Drawing.Size(200, 26);
             this.dtpDate.TabIndex = 14;
             // 
             // cmbPriority
             // 
+            this.cmbPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbPriority.FormattingEnabled = true;
-            this.cmbPriority.Location = new System.Drawing.Point(70, 66);
-            this.cmbPriority.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbPriority.Location = new System.Drawing.Point(105, 102);
             this.cmbPriority.Name = "cmbPriority";
-            this.cmbPriority.Size = new System.Drawing.Size(82, 21);
+            this.cmbPriority.Size = new System.Drawing.Size(121, 28);
             this.cmbPriority.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2, 69);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(3, 106);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(97, 20);
             this.label2.TabIndex = 16;
             this.label2.Text = "Приоритет:";
             // 
             // cmbFilter
             // 
+            this.cmbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbFilter.FormattingEnabled = true;
-            this.cmbFilter.Location = new System.Drawing.Point(70, 15);
-            this.cmbFilter.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbFilter.Location = new System.Drawing.Point(105, 23);
             this.cmbFilter.Name = "cmbFilter";
-            this.cmbFilter.Size = new System.Drawing.Size(90, 21);
+            this.cmbFilter.Size = new System.Drawing.Size(133, 28);
             this.cmbFilter.TabIndex = 17;
             // 
             // dgvTasks
@@ -168,55 +160,65 @@
             this.Date,
             this.Priority,
             this.Completed});
-            this.dgvTasks.Location = new System.Drawing.Point(345, 6);
+            this.dgvTasks.Location = new System.Drawing.Point(518, 9);
+            this.dgvTasks.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvTasks.Name = "dgvTasks";
-            this.dgvTasks.Size = new System.Drawing.Size(848, 269);
+            this.dgvTasks.RowHeadersWidth = 62;
+            this.dgvTasks.Size = new System.Drawing.Size(1272, 414);
             this.dgvTasks.TabIndex = 19;
             this.dgvTasks.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTasks_CellClick);
             // 
             // Title
             // 
             this.Title.HeaderText = "Название";
+            this.Title.MinimumWidth = 8;
             this.Title.Name = "Title";
             this.Title.Width = 150;
             // 
             // Description
             // 
             this.Description.HeaderText = "Описание";
+            this.Description.MinimumWidth = 8;
             this.Description.Name = "Description";
             this.Description.Width = 250;
             // 
             // Date
             // 
             this.Date.HeaderText = "Дата";
+            this.Date.MinimumWidth = 8;
             this.Date.Name = "Date";
             this.Date.Width = 150;
             // 
             // Priority
             // 
             this.Priority.HeaderText = "Приоритет";
+            this.Priority.MinimumWidth = 8;
             this.Priority.Name = "Priority";
+            this.Priority.Width = 150;
             // 
             // Completed
             // 
             this.Completed.HeaderText = "Готовность";
+            this.Completed.MinimumWidth = 8;
             this.Completed.Name = "Completed";
             this.Completed.Width = 150;
             // 
             // rtxtDescription
             // 
-            this.rtxtDescription.Location = new System.Drawing.Point(169, 51);
+            this.rtxtDescription.Location = new System.Drawing.Point(254, 78);
+            this.rtxtDescription.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.rtxtDescription.Name = "rtxtDescription";
-            this.rtxtDescription.Size = new System.Drawing.Size(141, 96);
+            this.rtxtDescription.Size = new System.Drawing.Size(210, 146);
             this.rtxtDescription.TabIndex = 20;
             this.rtxtDescription.Text = "";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(166, 35);
+            this.label4.Location = new System.Drawing.Point(249, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.Size = new System.Drawing.Size(87, 20);
             this.label4.TabIndex = 21;
             this.label4.Text = "Описание:";
             // 
@@ -230,9 +232,11 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.chkCompleted);
             this.groupBox1.Controls.Add(this.cmbPriority);
-            this.groupBox1.Location = new System.Drawing.Point(10, 6);
+            this.groupBox1.Location = new System.Drawing.Point(15, 9);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(319, 161);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(478, 248);
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Данные задачи";
@@ -242,9 +246,11 @@
             this.groupBox2.Controls.Add(this.btnAdd);
             this.groupBox2.Controls.Add(this.btnDelete);
             this.groupBox2.Controls.Add(this.btnEdit);
-            this.groupBox2.Location = new System.Drawing.Point(10, 173);
+            this.groupBox2.Location = new System.Drawing.Point(15, 266);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(144, 102);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(216, 157);
             this.groupBox2.TabIndex = 23;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Действия";
@@ -254,18 +260,21 @@
             this.groupBox3.Controls.Add(this.btnSort);
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.cmbFilter);
-            this.groupBox3.Location = new System.Drawing.Point(160, 173);
+            this.groupBox3.Location = new System.Drawing.Point(240, 266);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(169, 102);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(254, 157);
             this.groupBox3.TabIndex = 24;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Сортировка и фильтры";
             // 
             // btnSort
             // 
-            this.btnSort.Location = new System.Drawing.Point(10, 46);
+            this.btnSort.Location = new System.Drawing.Point(15, 71);
+            this.btnSort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSort.Name = "btnSort";
-            this.btnSort.Size = new System.Drawing.Size(150, 38);
+            this.btnSort.Size = new System.Drawing.Size(225, 58);
             this.btnSort.TabIndex = 19;
             this.btnSort.Text = "Примененить фильтры";
             this.btnSort.UseVisualStyleBackColor = true;
@@ -274,24 +283,24 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 18);
+            this.label3.Location = new System.Drawing.Point(10, 28);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.Size = new System.Drawing.Size(85, 20);
             this.label3.TabIndex = 18;
             this.label3.Text = "Фильтры:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1211, 291);
+            this.ClientSize = new System.Drawing.Size(1816, 448);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dgvTasks);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Text = "TaskManager - Управление задачами";
             ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).EndInit();
             this.groupBox1.ResumeLayout(false);
